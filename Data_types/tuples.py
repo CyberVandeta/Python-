61f5e10ec7f30858643334cf17184d6911ce6f30
CyberVandeta <kkruikar@gmail.com>
2020-07-12 15:46:29 +0530

    commit

---
+++ Data_types/tuples.py
@@ -0,0 +1,13 @@
+t = (1, 2, 3, 4, 5)
+t[0]
+print(t[1])
+
+dict = {1: 2, 2: 2, 3: 3}
+w = list(dict.items())
+print(w)
+# d = list((dict))
+# print(d)
+
+# f = [(3,2),(4,5),(6,7),(8,9)]
+# a = dict(f)
+# print(a)
