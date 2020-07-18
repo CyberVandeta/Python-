20c160a180d26ad72cd91dcd67b5ae7a97267e80
CyberVandeta <kkruikar@gmail.com>
2020-07-18 19:15:47 +0530

    Merge remote-tracking branch 'origin/master'
    * origin/master:
      commit

--- Object Oriented Programming/classes.py
+++ Object Oriented Programming/classes.py
@@ -1,12 +1,13 @@
 class Dog():
-    spesies ='mammal'
-    def __init__(self, breed, name, fur = True):
+    spesies = 'mammal'
+
+    def __init__(self, breed, name, fur=True):
         self.breed = breed
         self.name = name
         self.fur = fur
 
 
-sam = Dog(breed='Lab', name = 'sammy', fur=False)
+sam = Dog(breed='Lab', name='sammy', fur=False)
 print(sam.breed)
 print(sam.name)
 print(sam.spesies)
